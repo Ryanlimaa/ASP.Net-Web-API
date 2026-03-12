fad105aecd2444246e1c57943c46992a8352bc2f
ryan.limaa@outlook.com <ryan.limaa@outlook.com>
2026-03-12 18:01:21 -0300

    Vinculando Tarefas com Usuarios e fazendo a segunda migracao

--- SistemaDeTarefas/Migrations/20260312205116_VinculoTarefaUsuario.Designer.cs
+++ SistemaDeTarefas/Migrations/20260312205116_VinculoTarefaUsuario.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaDeTarefas.Data;
 
@@ -10,9 +11,11 @@ using SistemaDeTarefas.Data;
 namespace SistemaDeTarefas.Migrations
 {
     [DbContext(typeof(SistemaTarefaDBContext))]
-    partial class SistemaTarefaDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260312205116_VinculoTarefaUsuario")]
+    partial class VinculoTarefaUsuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SistemaDeTarefas/Model/TarefaModel.cs
+++ SistemaDeTarefas/Model/TarefaModel.cs
@@ -8,5 +8,9 @@ namespace SistemaDeTarefas.Model
         public string? Nome { get; set; }
         public string? Descricao { get; set; }
         public Status Status { get; set; }
+        public int UsuarioId { get; set; }
+
+        // Propriedade de navegação para o usuário associado à tarefa   
+        public virtual UsuarioModel? Usuario { get; set; }  
     }
 }
